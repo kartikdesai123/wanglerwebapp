ea78e77cc25ae21b676fc325e709a6aa921469de
Kartik Desai <kartikdesai123@gmail.com>
2020-11-09 22:45:18 +0530

    solve laravel issue

--- public/frontend/assets/js/customjs/product.js
+++ public/frontend/assets/js/customjs/product.js
@@ -18,7 +18,7 @@ var Product = function () {
                         temp_html = '<div class="product1 post7" style="margin: 10px">'+
                             '<figure class="p-img"><img src="' + base_url + "public/images/product/" + output[i].image + '" name="productimage"></figure>'+
                             '<h1 class="p-head">' + output[i].name.substr(0, 17) + '</h1>'+
-                            '<p class="p-info">' + output[i].description.substr(0, 17) + ' $' + output[i].price + '</p>'+
+                            '<p class="p-info">' + output[i].description.substr(0, 17) + ' </p>'+
                             '<p class="click"><img src="' + base_url + 'public/frontend/assets/images/small-leaf.png" alt="" /><a href="'+ base_url + "productdetails/"+ output[i].id +'">Click Here for more</a></p>'+
                         '</div>';
                         subcategoryhtml = subcategoryhtml + temp_html;
